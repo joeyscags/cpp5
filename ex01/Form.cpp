e8bd9dd9bbf652a02a5419f42b760fd7c02d5d98
joeyscags <joeyscags5000@gmail.com>
2026-02-01 20:59:08 +0100

    Add Form class implementation with exception handling and ostream overload

---
+++ ex01/Form.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 19:22:18 by joeyscags         #+#    #+#             */
+/*   Updated: 2026/02/01 20:58:24 by joeyscags        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form() : _name("default"), _isSigned(false), _gradeToSign(150), _gradeToExecute(150)
+{
+}
+
+Form::Form(const std::string& name, int gradeToSign, int gradeToExecute)
+	: _name(name), _isSigned(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute)
+{
+	if (gradeToSign < 1 || gradeToExecute < 1)
+		throw Form::GradeTooHighException();
+	if (gradeToSign > 150 || gradeToExecute > 150)
+		throw Form::GradeTooLowException();
+}
+
+Form::Form(const Form& other)
+	: _name(other._name), _isSigned(other._isSigned),
+	  _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute)
+{
+}
+
+Form& Form::operator=(const Form& other)
+{
+	if (this != &other)
+	{
+		_isSigned = other._isSigned;
+	}
+	return *this;
+}
+
+Form::~Form()
+{
+}
+
+const std::string& Form::getName() const
+{
+	return _name;
+}
+
+bool Form::getIsSigned() const
+{
+	return _isSigned;
+}
+
+int Form::getGradeToSign() const
+{
+	return _gradeToSign;
+}
+
+int Form::getGradeToExecute() const
+{
+	return _gradeToExecute;
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return "Grade is too high!";
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+	return "Grade is too low!";
+}
+
+std::ostream& operator<<(std::ostream& out, const Form& form)
+{
+	out << "Form " << form.getName()
+		<< ", signed: " << (form.getIsSigned() ? "yes" : "no")
+		<< ", grade required to sign: " << form.getGradeToSign()
+		<< ", grade required to execute: " << form.getGradeToExecute();
+	return out;
+}
