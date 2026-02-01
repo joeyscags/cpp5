a2870a01e2ff23a54ab6913838f76cc823932954
joeyscags <joeyscags5000@gmail.com>
2026-02-01 18:57:11 +0100

    Add Bureaucrat class implementation with exception handling and Makefile setup

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:47:29 by joeyscags         #+#    #+#             */
+/*   Updated: 2026/02/01 18:55:47 by joeyscags        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+
+class Bureaucrat
+{
+private:
+	const std::string	_name;
+	int					_grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string& name, int grade);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
+
+	const std::string&	getName() const;
+	int					getGrade() const;
+
+	void				incrementGrade();
+	void				decrementGrade();
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+};
+
+std::ostream&	operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
+
+#endif
