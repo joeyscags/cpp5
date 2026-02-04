16bcfe23e0b1d6da5cdd2248c0ad86f9a7d20f8a
joeyscags <joeyscags5000@gmail.com>
2026-02-04 23:06:44 +0100

    Add ShrubberyCreationForm class implementation with executeAction method

---
+++ ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 11:30:15 by joeyscags         #+#    #+#             */
+/*   Updated: 2026/02/04 23:06:32 by joeyscags        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+# include "AForm.hpp"
+# include <string>
+
+class ShrubberyCreationForm : public AForm
+{
+private:
+	std::string	_target;
+
+public:
+	ShrubberyCreationForm();
+	ShrubberyCreationForm(const std::string& target);
+	ShrubberyCreationForm(const ShrubberyCreationForm& other);
+	ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
+	~ShrubberyCreationForm();
+
+	virtual void	executeAction() const;
+};
+
+#endif
