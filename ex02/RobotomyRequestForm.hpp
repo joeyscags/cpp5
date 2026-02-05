08e00453f17bcdc2ba42315cb9f37f0d74df9af9
joeyscags <joeyscags5000@gmail.com>
2026-02-05 14:57:45 +0100

    Add RobotomyRequestForm class implementation with executeAction method

---
+++ ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 14:22:10 by joeyscags         #+#    #+#             */
+/*   Updated: 2026/02/05 14:57:30 by joeyscags        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+
+# include "AForm.hpp"
+# include <string>
+
+class RobotomyRequestForm : public AForm
+{
+private:
+	std::string	_target;
+
+public:
+	RobotomyRequestForm();
+	RobotomyRequestForm(const std::string& target);
+	RobotomyRequestForm(const RobotomyRequestForm& other);
+	RobotomyRequestForm& operator=(const RobotomyRequestForm& other);
+	~RobotomyRequestForm();
+
+	virtual void	executeAction() const;
+};
+
+#endif
